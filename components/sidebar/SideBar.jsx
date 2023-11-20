0561f42867c7d42832610d30fde2bbd7028a80fb
Morces <karanim594@gmail.com>
2023-11-20 12:36:18 +0300

    made sidebar updates

--- components/sidebar/SideBar.jsx
+++ components/sidebar/SideBar.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
 import { View, TouchableOpacity, Text, Image, Linking } from "react-native";
 import styles from "./sidebar.style";
-import { AntDesign } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
-const SideBar = ({ setOpen }) => {
+const SideBar = () => {
   const router = useRouter();
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.closeIcon} onPress={() => setOpen(false)}>
-        <AntDesign name="leftcircleo" size={24} color="black" />
-      </TouchableOpacity>
       <View style={styles.navContainer}>
         <TouchableOpacity
           style={styles.navTab}
-          onPress={() => router.push('profile')}
+          onPress={() => router.push("profile")}
         >
           <Text style={styles.navText}>Profile</Text>
         </TouchableOpacity>
